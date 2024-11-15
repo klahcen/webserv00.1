f41648b140554110ce7cd7b14856e33166769edc
Lahcen Kazaz <lkazaz@c3r6p6.1337.ma>
2024-11-15 13:02:56 +0100

    commit number 3

---
+++ serv.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+
+int main() {
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+
+    // HTTP response message
+    char *http_response =
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type: text/plain\r\n"
+        "Content-Length: 13\r\n"
+        "Connection: close\r\n"
+        "\r\n"
+        "Hello, World!";
+
+    // Create socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Set server address and port
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    // Bind the socket to the address
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Start listening for incoming connections
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("HTTP server listening on http://localhost:%d\n", PORT);
+
+    // Main server loop
+    while (1) {
+        // Accept a new connection
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
+            perror("accept");
+            close(server_fd);
+            exit(EXIT_FAILURE);
+        }
+
+        // Read the request (this example doesn't actually use the request)
+        read(new_socket, buffer, 1024);
+        printf("Received request:\n%s\n", buffer);
+
+        // Send HTTP response
+        write(new_socket, http_response, strlen(http_response));
+
+        // Close the connection
+        close(new_socket);
+    }
+
+    return 0;
+}
---
+++ serv.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <cstring>
+
+#define PORT 8080
+
+int main() {
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+
+    // Step 1: Create a socket
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == 0) {
+        std::cerr << "Socket failed" << std::endl;
+        return -1;
+    }
+
+    // Step 2: Bind the socket to a port
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        std::cerr << "Bind failed" << std::endl;
+        close(server_fd);
+        return -1;
+    }
+
+    // Step 3: Listen for incoming connections
+    if (listen(server_fd, 3) < 0) {
+        std::cerr << "Listen failed" << std::endl;
+        close(server_fd);
+        return -1;
+    }
+
+    std::cout << "Server is listening on port " << PORT << std::endl;
+
+    // Step 4: Accept a connection and send an HTTP response
+    while (1) {
+        new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
+        if (new_socket < 0) {
+            std::cerr << "Accept failed" << std::endl;
+            close(server_fd);
+            return -1;
+        }
+
+        const char *http_response =
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: text/plain\r\n"
+            "Content-Length: 13\r\n"
+            "Connection: close\r\n"
+            "\r\n"
+            "Hello,";
+
+        send(new_socket, http_response, strlen(http_response), 0);
+        close(new_socket);
+    }
+
+    close(server_fd);
+    return 0;
+}
